806ac49145739df6561a697d0bdc3cc685dfdce0
ckolin <25738463+ckolin@users.noreply.github.com>
2021-12-17 15:57:50 +0100

    Calculate fitness concurrently

--- pop.go
+++ pop.go
@@ -7,14 +7,15 @@ import (
 	"math"
 	"math/rand"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/fogleman/gg"
 )
 
-const populationSize = 300
+const populationSize = 1000
 const geneLength = 6
-const mutationRate = 0.03
+const mutationRate = 0.01
 const generations = 16384
 
 type Shape struct {
@@ -22,9 +23,61 @@ type Shape struct {
 	r, g, b    float64
 }
 
+func generateShape() Shape {
+	return Shape{
+		x: rand.Float64(),
+		y: rand.Float64(),
+		w: rand.Float64(),
+		h: rand.Float64(),
+		r: rand.Float64(),
+		g: rand.Float64(),
+		b: rand.Float64(),
+	}
+}
+
+func (s *Shape) Mutate() {
+	if rand.Float64() < mutationRate {
+		mutateFloat(&s.x)
+	}
+	if rand.Float64() < mutationRate {
+		mutateFloat(&s.y)
+	}
+	if rand.Float64() < mutationRate {
+		mutateFloat(&s.w)
+	}
+	if rand.Float64() < mutationRate {
+		mutateFloat(&s.h)
+	}
+	if rand.Float64() < mutationRate {
+		mutateFloat(&s.r)
+	}
+	if rand.Float64() < mutationRate {
+		mutateFloat(&s.g)
+	}
+	if rand.Float64() < mutationRate {
+		mutateFloat(&s.b)
+	}
+}
+
+func mutateFloat(f *float64) {
+	*f = math.Max(0, math.Min(1, *f+(rand.Float64()-0.5)))
+}
+
 type Dna struct {
 	genes []Shape
 	f     float64
+}
+
+func generateDna() Dna {
+	dna := Dna{
+		genes: make([]Shape, geneLength),
+	}
+
+	for i := range dna.genes {
+		dna.genes[i] = generateShape()
+	}
+
+	return dna
 }
 
 func (a *Dna) Combine(b *Dna) Dna {
@@ -32,8 +85,10 @@ func (a *Dna) Combine(b *Dna) Dna {
 		genes: make([]Shape, len(a.genes)),
 	}
 
+	fac := rand.Float64()
+
 	for i := 0; i < geneLength; i++ {
-		if rand.Float64() < 0.5 {
+		if rand.Float64() < fac {
 			c.genes[i] = a.genes[i]
 		} else {
 			c.genes[i] = b.genes[i]
@@ -45,45 +100,21 @@ func (a *Dna) Combine(b *Dna) Dna {
 
 func (dna *Dna) Mutate() {
 	for i := range dna.genes {
-		if rand.Float64() < mutationRate {
-			mutateFloat(&dna.genes[i].x)
-		}
-		if rand.Float64() < mutationRate {
-			mutateFloat(&dna.genes[i].y)
-		}
-		if rand.Float64() < mutationRate {
-			mutateFloat(&dna.genes[i].w)
-		}
-		if rand.Float64() < mutationRate {
-			mutateFloat(&dna.genes[i].h)
-		}
-		if rand.Float64() < mutationRate {
-			mutateFloat(&dna.genes[i].r)
-		}
-		if rand.Float64() < mutationRate {
-			mutateFloat(&dna.genes[i].g)
-		}
-		if rand.Float64() < mutationRate {
-			mutateFloat(&dna.genes[i].b)
-		}
+		dna.genes[i].Mutate()
 	}
-}
-
-func mutateFloat(f *float64) {
-	*f = math.Max(0, math.Min(1, *f+(rand.Float64()-0.5)))
 }
 
 func (dna *Dna) Render(dc *gg.Context) {
 	bounds := dc.Image().Bounds()
-	for i := range dna.genes {
-		shape := dna.genes[i]
+	dc.Scale(float64(bounds.Dx()), float64(bounds.Dy()))
+	for _, shape := range dna.genes {
 		dc.DrawRectangle(
-			(shape.x-shape.w/2.0)*float64(bounds.Dx()),
-			(shape.y-shape.h/2.0)*float64(bounds.Dy()),
-			shape.w*float64(bounds.Dx()),
-			shape.h*float64(bounds.Dy()),
+			shape.x-shape.w/2.0,
+			shape.y-shape.h/2.0,
+			shape.w,
+			shape.h,
 		)
-		dc.SetRGBA(shape.r, shape.g, shape.b, 0.5)
+		dc.SetRGBA(shape.r, shape.g, shape.b, 0.6)
 		dc.Fill()
 	}
 }
@@ -97,10 +128,11 @@ func main() {
 	}
 	bounds := goal.Bounds()
 
-	// Create starting canvas
+	// Create blank image
 	dc := gg.NewContext(bounds.Dx(), bounds.Dy())
 	dc.SetRGB(0.5, 0.5, 0.5)
 	dc.Clear()
+	blank := dc.Image()
 
 	rand.Seed(time.Now().UTC().UnixNano())
 
@@ -112,19 +144,29 @@ func main() {
 
 	for gen := 1; gen <= generations; gen++ {
 		// Calculate fitness
+		var wg sync.WaitGroup
+		wg.Add(len(pop))
+
 		total := 0.0
 		best := 0.0
 		best_i := 0
+
 		for i := range pop {
-			test := gg.NewContextForImage(dc.Image())
-			pop[i].Render(test)
-			pop[i].f = fitness(test.Image(), goal)
-			if pop[i].f > best {
-				best = pop[i].f
-				best_i = i
-			}
-			total += pop[i].f
+			go func(i int) {
+				defer wg.Done()
+				test := gg.NewContextForImage(blank)
+				pop[i].Render(test)
+				pop[i].f = fitness(test.Image(), goal)
+
+				if pop[i].f > best {
+					best = pop[i].f
+					best_i = i
+				}
+				total += pop[i].f
+			}(i)
 		}
+
+		wg.Wait()
 
 		// Arrange for selection
 		sum := 0.0
@@ -157,30 +199,6 @@ func parseArgs() string {
 	inPtr := flag.String("i", "in.jpg", "input file")
 	flag.Parse()
 	return *inPtr
-}
-
-func generateDna() Dna {
-	dna := Dna{
-		genes: make([]Shape, geneLength),
-	}
-
-	for i := range dna.genes {
-		dna.genes[i] = generateShape()
-	}
-
-	return dna
-}
-
-func generateShape() Shape {
-	return Shape{
-		x: rand.Float64(),
-		y: rand.Float64(),
-		w: rand.Float64(),
-		h: rand.Float64(),
-		r: rand.Float64(),
-		g: rand.Float64(),
-		b: rand.Float64(),
-	}
 }
 
 func pick(pop []Dna) *Dna {
